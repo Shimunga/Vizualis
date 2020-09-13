cc48dd4eca9efe6eb6782a2af0c7c8a11bdffb94
Leo <shimunga@hotmail.com>
2020-09-13 11:45:09 +0300

    Dialogs multi +custom started

--- app/src/main/java/com/example/vizualis/AlertsTabsPagesActivity.kt
+++ app/src/main/java/com/example/vizualis/AlertsTabsPagesActivity.kt
@@ -1,5 +1,6 @@
 package com.example.vizualis
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -21,12 +22,56 @@ class AlertsTabsPagesActivity : AppCompatActivity() {
          .setPositiveButton("ok") { dialog, id ->
             Toast.makeText(
                this,
-               "Ok clicked",
+               "Simple dialog Ok clicked",
                Toast.LENGTH_SHORT
             ).show()
          }
-         .setNegativeButton("cancel") { _, _ -> }
+         .setNegativeButton("cancel"){ dialog, id -> onNegativeClick(dialog, id)}
          .setNeutralButton("remind me later") { _, _ -> }
+      val dialog = builder.create()
+      dialog.show()
+   }
+
+   fun onNegativeClick(dialog: DialogInterface, which: Int){
+      Toast.makeText(
+         this,
+         "Simple dialog Nok clicked",
+         Toast.LENGTH_SHORT
+      ).show()
+   }
+
+   fun onClickListDialogBtn(view: View) {
+      val builder = AlertDialog.Builder(this)
+      val products = arrayOf("Bread", "Eggs", "Milk", "Potatoes")
+      builder.setTitle("Choose product")
+         .setItems(products) { dialog, which ->
+            Toast.makeText(
+               this,
+               "List dialog clicked: ${products[which]}",
+               Toast.LENGTH_SHORT
+            ).show()
+         }
+      val dialog = builder.create()
+      dialog.show()
+   }
+
+   fun onClickMultiChoiceDialogBtn(view: View) {
+      val builder = AlertDialog.Builder(this)
+      val products = arrayOf("Bread", "Eggs", "Milk", "Potatoes")
+      val checked = booleanArrayOf(true, true, false, true)
+      val selectedItems = mutableListOf<Int>()
+      builder
+         .setTitle("Choose product")
+         .setMultiChoiceItems(products,checked) { dialog, which, isChecked ->
+            if (isChecked) selectedItems.add(which) else selectedItems.remove(which)
+         }
+         .setPositiveButton("ok") { dialog, id ->
+            Toast.makeText(
+               this,
+               selectedItems.toString(),
+               Toast.LENGTH_SHORT
+            ).show()
+         }
       val dialog = builder.create()
       dialog.show()
    }
